912c13f28d6112ccd406c5d6b48152d005cdee9c
josephparis97 <joseph.paris@esme.fr>
2023-04-24 21:35:34 +0200

    add header generator

--- backend/hashtaggen_api.py
+++ backend/hashtaggen_api.py
@@ -1,9 +1,18 @@
 from hashtaggen import getHashtags
 from fastapi import FastAPI, HTTPException
 from mangum import Mangum
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 handler = Mangum(app)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/getHashtags")
 async def get_hashtags_route(userInput: str):
